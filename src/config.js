82dfbf20d4c2dd14fce265703ab91873bb65af1c
Deepanshu <deep.nalhera476@gmail.com>
2026-09-09 16:34:22 +0530

    Update client plans page and chat upload origins

--- src/config.js
+++ src/config.js
@@ -13,7 +13,11 @@
 export const API_URL = 'http://127.0.0.1:3000/api'
 export const API_URL_IMG = 'http://127.0.0.1:3000/'
 export const SOCKET_URL = 'http://127.0.0.1:3000'
-
+export const CHAT_UPLOAD_ORIGINS = [
+  API_URL_IMG,
+  'https://backendappapp.ciisnetwork.in/',
+  'https://backendcds.ciisnetwork.in/',
+]
 
 export const TURN_URL = import.meta.env.VITE_TURN_URL || ''
 export const TURN_USERNAME = import.meta.env.VITE_TURN_USERNAME || ''
